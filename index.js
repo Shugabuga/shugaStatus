6fb6f81c46a78b3e131830bc2ea1c691b1a0ba25
Andre <6000071+uintdev@users.noreply.github.com>
2019-10-18 00:47:56 +0100

    Improve readability
    Added spaces where appropriate.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function shugaStatus() {
     this.compile = new function() {
-        this.monitor = (key,category) => {
+        this.monitor = (key, category) => {
             const req = new XMLHttpRequest();
-            req.open("POST","https://api.uptimerobot.com/v2/getMonitors",true);
+            req.open("POST","https://api.uptimerobot.com/v2/getMonitors", true);
             req.setRequestHeader("Content-Type", "application/json");
             req.onload = () => {
                 let resp = JSON.parse(req.responseText);
@@ -34,15 +34,15 @@ function shugaStatus() {
                 }
 
                 let badge = "";
-                if(obj.status == 9 || obj.status == 8) {
+                if (obj.status == 9 || obj.status == 8) {
                     badge = `<span class="badge badge-secondary bg-${status.toLowerCase()}">!</span>`;
-                } else if(obj.status != 2) {
+                } else if (obj.status != 2) {
                     badge = `<span class="badge badge-secondary">?</span>`;
                 }
 
                 ShugaStatus.state.update();
                 let out = `<div class="monitor">
-                        <h2>${badge} <a class="online-name hide-link" href="${link}" target="_blank">${obj.friendly_name}</a> <div class="online-status text-${status.toLowerCase()} right">${status}</div></h2>
+                        <h2>${badge} <a class="online-name hide-link" href="${link}" target="_blank" rel="noopener">${obj.friendly_name}</a> <div class="online-status text-${status.toLowerCase()} right">${status}</div></h2>
                         <div class="row online-details">
                             <div class="col-md-4">
                                 <div class="progress-today-percent right">${Math.round(daily*100)/100}%</div>
@@ -84,7 +84,7 @@ function shugaStatus() {
         for (cat in data) {
             ShugaStatus.compile.entry(cat);
             for (let i = 0; i < data[cat].length; i++) {                
-                ShugaStatus.compile.monitor(data[cat][i],cat);
+                ShugaStatus.compile.monitor(data[cat][i], cat);
             }
         }
         ShugaStatus.news();
@@ -97,22 +97,22 @@ function shugaStatus() {
     }
     this.refresh = () => {
         ShugaStatus.deinit();
-        window.setTimeout(ShugaStatus.init,1000);
+        window.setTimeout(ShugaStatus.init, 1000);
     }
     this.state = new function() {
         this.isOffline = 0;
         this.isIntermittent = 0;
         this.update = () => {
-            if(ShugaStatus.state.isOffline > 1) {
+            if (ShugaStatus.state.isOffline > 1) {
                 document.getElementById("tldr").innerHTML = ShugaStatus.state.isOffline + " services are currently suffering down-time.";
                 document.getElementById("tldr").className = "alert alert-danger";
-            } else if(ShugaStatus.state.isIntermittent > 1) {
+            } else if (ShugaStatus.state.isIntermittent > 1) {
                 document.getElementById("tldr").innerHTML = ShugaStatus.state.isIntermittent + " services are currently suffering some interruptions.";
                 document.getElementById("tldr").className = "alert alert-warning";
-            } else if(ShugaStatus.state.isOffline == 1) {
+            } else if (ShugaStatus.state.isOffline == 1) {
                 document.getElementById("tldr").innerHTML = ShugaStatus.state.isOffline + " service is currently suffering down-time.";
                 document.getElementById("tldr").className = "alert alert-danger";
-            } else if(ShugaStatus.state.isIntermittent == 1) {
+            } else if (ShugaStatus.state.isIntermittent == 1) {
                 document.getElementById("tldr").innerHTML = ShugaStatus.state.isIntermittent + " service is currently suffering some interruptions.";
                 document.getElementById("tldr").className = "alert alert-warning";
             } else {
@@ -136,7 +136,7 @@ function shugaStatus() {
                 </div>`
                 n++;
             }
-            if(n == 0) {
+            if (n == 0) {
                 news.innerHTML = `<div class="news news-empty">
                     There is no news to display!
                 </div>`;
@@ -148,13 +148,13 @@ function shugaStatus() {
         const el = document.querySelector(".progress-master");
         value = Number(el.getAttribute("timer"));
         value--;
-        if(value <= 0) {
+        if (value <= 0) {
             ShugaStatus.refresh();
             value = 300;
         }
-        el.setAttribute("timer",value);
+        el.setAttribute("timer", value);
         let seconds = Math.round(value%60);
-        if(seconds < 10) {
+        if (seconds < 10) {
             seconds = "0" + String(seconds);
         }
         el.innerHTML = "Refreshing in " + Math.floor(value/60) + ":" + seconds;
@@ -167,9 +167,9 @@ const ShugaStatus = new shugaStatus();
 
 // Manipulate data here. Object with arrays of API tokens.
 const data = {
-    "My Category": ["m778874106-d364e78085e7c4a22ff7d38a","m778959290-aec903f88269aa3621ba3ad5","m778874126-2199efa94e371583fd42a7e7"],
+    "My Category": ["m778874106-d364e78085e7c4a22ff7d38a", "m778959290-aec903f88269aa3621ba3ad5", "m778874126-2199efa94e371583fd42a7e7"],
     "Repos": ["m782032055-ca8a6afe19b12773f1a64afb", "m782032070-381a2752b20465d844ce5182", "m782032075-c1a488d7daf392a9d7bd475a", "m782032076-509c8f5e5ea7bc6958eabd62"]
 }
 
-document.body.addEventListener("load",ShugaStatus.init());
-window.setInterval(ShugaStatus.progress,1000);
+document.body.addEventListener("load", ShugaStatus.init());
+window.setInterval(ShugaStatus.progress, 1000);
